891fbb762c5e79fe84c362eb8366dd5ac6f93cd4
Glen <glen.adams@gmail.com>
2019-04-07 20:48:21 +0200

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('Show a form here!')
+      )
+    );
+  }
+}
